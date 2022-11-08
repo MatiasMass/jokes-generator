2dc9758f2aea460442b0751589890b5ed73c317b
MatiasMass <matiasmass14@gmail.com>
2022-11-08 18:46:12 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 import './App.css';
+import button from "./images/icon-dice.svg"
+import separator from './images/pattern-divider-desktop.svg'
+
+const baseUrl = "https://sv443.net/jokeapi/v2/joke/Programming?type=single"
 
 function App() {
+  const [id, setId] = useState("")
+  const [joke, setJoke] = useState("")
+
+
+  const getAdvice = () => {
+    const request = axios.get(baseUrl)
+    console.log('request', request);
+    return request.then(response => response.data)
+  }
+  
+
+  
+  const jokeAPI = async () =>{
+    let arrayOfJokes = []
+    try{
+      const data = await axios.get(baseUrl)
+      console.log(data);
+      arrayOfJokes = data.data
+    } catch (error){
+      console.log(error);
+    }
+
+    try {
+      setId(arrayOfJokes.id)
+      setJoke(arrayOfJokes.joke)
+      console.log('test', arrayOfJokes);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    // getAdvice()
+    //   .then(initialAdvice => {
+    //     setId(initialAdvice.id)
+    //     setJoke(initialAdvice.joke)
+    //   })
+    jokeAPI()
+  }, [])
+
+  // // axiosApi();
+  // // console.log(advice);
+  // getAdvice()
+  // console.log(advice, id);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h3>Joke #{id}</h3>
+        <p>"{joke}"</p>
+        <div className="separator">
+          <img src={separator} alt="" />
+        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={jokeAPI}><img src={button} alt="button" /></button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+  
+// const rootElement = document.getElementById("root");
+  
+// // Create a functional component
+// const App = () => {
+//     // Declare a state variable
+//     const [Joke, setJoke] = React.useState("");
+  
+//     const fetchApi = () => {
+//         // Fetching data from the API
+//             fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+//             .then((res) => res.json())
+//             .then((data) => setJoke(data.joke));
+//       };
+  
+//     return (
+//         // Return the Button Component with a conditional statement
+//         <div>{Joke === "" ? <Button callApi={fetchApi} /> : 
+// <p>{Joke}</p>
+// }</div>
+//       );
+// }
+  
+// // Rendering the App Component.
+// ReactDOM.render(
+//     <App />,
+//   rootElement
+// );
+// export default App
